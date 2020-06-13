7e71dd3ab05e34fddff185417e8a916754d6e506
Francis Gonzalez <Kitomc.rd@gmail.com>
2020-06-14 06:00:46 +0800

    Bases Del Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ColaComponent } from './componentes/cola/cola.component';
+import { ColaUsuarioComponent } from './componentes/cola-usuario/cola-usuario.component';
+
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider, FacebookLoginProvider } from "angularx-social-login";
+ 
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("312465983373-t99k5hbddul1e0unho32i0gn1ma96gir.apps.googleusercontent.com")
+  },
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("1169581540072333")
+  }
+]);
+export function provideConfig() {
+  return config;
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ColaComponent,
+    ColaUsuarioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [{
+    provide: AuthServiceConfig,
+    useFactory: provideConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/componentes/cola-usuario/cola-usuario.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cola-usuario',
+  templateUrl: './cola-usuario.component.html',
+  styleUrls: ['./cola-usuario.component.css']
+})
+export class ColaUsuarioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
