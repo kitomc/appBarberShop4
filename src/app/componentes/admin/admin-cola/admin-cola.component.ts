5b712062334ca7e48fd54bfc1f07f61c4bff0aad
Francis Gonzalez <Kitomc.rd@gmail.com>
2020-06-15 21:36:39 -0400

    Parte administrtiva  Template de ejemplo

---
+++ src/app/componentes/admin/admin-cola/admin-cola.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-cola',
+  templateUrl: './admin-cola.component.html',
+  styleUrls: ['./admin-cola.component.css']
+})
+export class AdminColaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
